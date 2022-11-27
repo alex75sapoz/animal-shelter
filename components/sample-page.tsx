77b7879e11aa378a20bf797c94d1e8c07991834d
Alex <alexei.sapoz@gmail.com>
2022-11-27 04:36:17 -0600

    Small commit

--- components/sample-page.tsx
+++ components/sample-page.tsx
@@ -5,7 +5,7 @@ export default function SamplePage({name}: {
     <div className='flex justify-center items-center h-80 border-2 border-dashed text-center align-middle border-sky-500'>
       <div className='flex-col'>
         <p className='text-4xl font-bold'>{name}</p>
-        <p>Add Content Here </p>
+        <p>Add Content Here</p>
       </div>
     </div>
   )
