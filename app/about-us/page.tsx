8b72dbb2030bafe4afecf71cfd1970ca21d618da
Alex <alexei.sapoz@gmail.com>
2022-11-27 05:11:39 -0600

    Testing

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,10 +1,7 @@
+import SamplePage from '../../components/sample-page'
+
 export default function Page() {
   return (
-    <div className='flex justify-center items-center h-80 border-2 border-dashed text-center align-middle border-sky-500'>
-      <div className='flex-col'>
-        <p className='text-4xl font-bold'>About Us</p>
-        <p>Add Content Here</p>
-      </div>
-    </div>
+    <SamplePage name='About Us' />
   )
 }
